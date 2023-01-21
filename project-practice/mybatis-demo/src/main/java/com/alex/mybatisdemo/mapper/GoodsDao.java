88c44338a2bd2f87d66d410f76b7640bed0a2270
非洲平头哥 <1401527242@qq.com>
2023-01-21 10:08:24 +0800

    完成单表的增删改查及单元测试

--- project-practice/mybatis-demo/src/main/java/com/alex/mybatisdemo/mapper/GoodsDao.java
+++ project-practice/mybatis-demo/src/main/java/com/alex/mybatisdemo/mapper/GoodsDao.java
@@ -3,6 +3,8 @@ package com.alex.mybatisdemo.mapper;
 import com.alex.mybatisdemo.entity.GoodsDo;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author alex
  * @version 1.0
@@ -17,4 +19,29 @@ public interface GoodsDao {
      * @return
      */
     public int insert(GoodsDo goods);
+
+    /**
+     * 删除商品(根据id)*
+     */
+    public int delete(Long id);
+
+    /**
+     * 修改商品信息(根据id修改其他属性值) *
+     * @param goods
+     * @return
+     */
+    public int update(GoodsDo goods);
+
+    /**
+     * 根据id选择商品 *
+     * @param id
+     * @return
+     */
+    public GoodsDo selectOne(Long id);
+
+    /**
+     * 查询所有商品 *
+     * @return
+     */
+    public List<GoodsDo> selectAll();
 }
--- project-practice/mybatis-demo/src/test/java/com/alex/mybatisdemo/MybatisDemoApplicationTests.java
+++ project-practice/mybatis-demo/src/test/java/com/alex/mybatisdemo/MybatisDemoApplicationTests.java
@@ -2,15 +2,21 @@ package com.alex.mybatisdemo;
 
 import com.alex.mybatisdemo.entity.GoodsDo;
 import com.alex.mybatisdemo.mapper.GoodsDao;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MybatisDemoApplicationTests {
 
     @Autowired
@@ -20,7 +26,13 @@ class MybatisDemoApplicationTests {
     void contextLoads() {
     }
 
+    private Long currentId=8L;
+
+    /**
+     * 新增商品 *
+     */
     @Test
+    @Order(1)
     void test_01(){
         GoodsDo goods = new GoodsDo();
         goods.setName("手机1");
@@ -29,7 +41,51 @@ class MybatisDemoApplicationTests {
         goods.setCreateTime(new Date());
         int count = goodsDao.insert(goods);
         assertEquals(1,count);
+    }
 
+    /**
+     * 更新商品信息*
+     */
+    @Test
+    @Order(2)
+    void test_02(){
+        GoodsDo goods = new GoodsDo();
+        goods.setId(currentId);
+        goods.setName("手机1");
+        goods.setPic("phone1.jpg");
+        goods.setPrice("4000");
+        goods.setCreateTime(new Date());
+        int count = goodsDao.update(goods);
+        assertEquals(1,count);
+    }
+
+    /**
+     * 获取商品信息 *
+     */
+    @Test
+    @Order(3)
+    void test_03(){
+        GoodsDo goods = goodsDao.selectOne(currentId);
+        assertNotNull(goods);
+    }
+
+    /**
+     * 删除商品 *
+     */
+    @Test
+    @Order(4)
+    void test_04(){
+        int count = goodsDao.delete(currentId);
+        assertEquals(1,count);
+    }
+
+    /**
+     * 获取商品列表 *
+     */
+    @Test
+    void test_05(){
+        List<GoodsDo> goodsList = goodsDao.selectAll();
+        assertEquals(0,goodsList.size());
     }
 
 }
